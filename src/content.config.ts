5de3cd26f208d6a37ac3f35eb89974492d792b33
Olle Korkmaz Zillén <olle.korkmaz-zillen@airahome.com>
2026-01-13 08:29:34 +0100

    renovating

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,20 +9,15 @@ const blog = defineCollection({
             title: z.string(),
             description: z.string(),
             pubDate: z.coerce.date(),
-
-            // if you really want string, keep it; otherwise make it date like pubDate
             updatedDate: z.string().optional(),
-
             heroImage: image().optional(),
             badge: z.string().optional(),
-
             tags: z
                 .array(z.string())
                 .refine((items) => new Set(items).size === items.length, {
                     message: "tags must be unique",
                 })
                 .optional(),
-
             video: z.string().optional(),
         }),
 });
