8dad1881b40f5ac0aa2233a61b7391066f7a21d9
Olle Korkmaz Zillén <olle.korkmaz-zillen@airahome.com>
2026-01-13 08:27:59 +0100

    renovating

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,25 +3,28 @@ import { defineCollection } from "astro:content"
 import { z } from "astro/zod"
 
 const blog = defineCollection({
-    loader: glob({ pattern: '**\/[^_]*.mdx', base: "./blog" }),
+    loader: glob({ pattern: "**/[^_]*.mdx", base: "./blog" }),
     schema: ({ image }) =>
         z.object({
             title: z.string(),
             description: z.string(),
             pubDate: z.coerce.date(),
-            updatedDate: z.optional(z.string()),
-            heroImage: z.optional(image()),
-            badge: z.optional(z.string()),
+
+            // if you really want string, keep it; otherwise make it date like pubDate
+            updatedDate: z.string().optional(),
+
+            heroImage: image().optional(),
+            badge: z.string().optional(),
+
             tags: z
-                .optional(
-                    z.array(z.string()).refine((items) => new Set(items).size === items.length, {
-                        message: 'tags must be unique',
-                    })
-                ),
-            video: z.optional(z.string()),
+                .array(z.string())
+                .refine((items) => new Set(items).size === items.length, {
+                    message: "tags must be unique",
+                })
+                .optional(),
+
+            video: z.string().optional(),
         }),
 });
 
-export const collections = {
-    blog
-};
+export const collections = { blog };
